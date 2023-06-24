30cabb5808b0497e396c7aa07a2e4d909329ac96
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-24 05:42:23 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ const App = () => {
   const tabs = [
     {
       title: 'Tab 1',
-      content: 'This is the content for Tab1',
+      content: 'This is the content for Tab 1.',
     },
     {
       title: 'Tab 2',
-      content: 'This is the content for Tab2',
+      content: 'This is the content for Tab 2.',
     },
     {
       title: 'Tab 3',
-      content: 'This is the content for Tab3',
+      content: 'This is the content for Tab 3.',
     },
   ];
 

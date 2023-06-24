3a679f46940e6ee4b1880a49fe33b69a892748e3
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-24 05:40:31 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Tabs from './Tabs';
 
 const App = () => {
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: 'This is the content for Tab1',
+    },
+    {
+      title: 'Tab 2',
+      content: 'This is the content for Tab2',
+    },
+    {
+      title: 'Tab 3',
+      content: 'This is the content for Tab3',
+    },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
